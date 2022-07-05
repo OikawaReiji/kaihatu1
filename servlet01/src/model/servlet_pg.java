67cddfe62d23f46c1470ce3e70855504ab60549b
komai012 <k.komai.sys21@morijyobi.ac.jp>
2022-07-05 10:40:45 +0900

    1回目のコミット

--- servlet01/src/model/servlet_pg.java
+++ servlet01/src/model/servlet_pg.java
@@ -1,19 +1,17 @@
 package model;
 
 import java.io.IOException;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-/**
- * Servlet implementation class servlet_pg
- */
 @WebServlet("/servlet_pg")
 public class servlet_pg extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-       
+
     /**
      * @see HttpServlet#HttpServlet()
      */
@@ -22,9 +20,6 @@ public class servlet_pg extends HttpServlet {
         // TODO Auto-generated constructor stub
     }
 
-	/**
-	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
-	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
 		response.getWriter().append("Served at: ").append(request.getContextPath());
